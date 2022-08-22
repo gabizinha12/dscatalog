07d2d7ce01f61ca4759decdd2315865bdab77782
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-08-22 01:42:19 -0300

    H2 Database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository; // dependencia da camada de serviço
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+}
