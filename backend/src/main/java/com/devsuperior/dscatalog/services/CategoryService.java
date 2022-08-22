7a89db3c4e5576228d2457a8b558fcade5098c8a
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-08-22 15:33:28 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -12,7 +13,9 @@ import com.devsuperior.dscatalog.repositories.CategoryRepository;
 public class CategoryService {
 
 	@Autowired
-	private CategoryRepository repository; // dependencia da camada de serviço
+	private CategoryRepository repository;// dependencia da camada de serviço
+	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
