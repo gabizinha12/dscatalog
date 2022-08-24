86585bd2a08e04a08db1312a7f19ee5f9e0d0e87
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-08-24 01:21:04 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dscatalog.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException{
 
 	/**
 	 * 
@@ -8,7 +8,7 @@ public class EntityNotFoundException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 
 	
-	public EntityNotFoundException(String msg) {
+	public ResourceNotFoundException(String msg) {
 		super(msg);
 	}
 }
